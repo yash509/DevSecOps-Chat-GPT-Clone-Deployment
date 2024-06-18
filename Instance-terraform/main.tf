b0c4662a3a8eac3eac4afc226c3854ecabe6749d
Yash Varma <71536409+yash509@users.noreply.github.com>
2024-06-18 07:40:11 +0530

    Update main.tf

--- Instance-terraform/main.tf
+++ Instance-terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "Jenkins-sg" {
-  name        = "Jenkins-Security Group"
+  name        = "Jenkins-SG"
   description = "Open 22,443,80,8080,9000,9100,9090,3000"
 
   # Define a single ingress rule to allow traffic on all specified ports
@@ -33,7 +33,7 @@ resource "aws_security_group" "Jenkins-sg" {
 resource "aws_instance" "web" {
   ami                    = "ami-0f58b397bc5c1f2e8"  #change your ami value according to your aws instance
   instance_type          = "t2.large"
-  key_name               = "maylatest"
+  key_name               = "" #your key name here
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./script.sh", {})
 
@@ -47,7 +47,7 @@ resource "aws_instance" "web" {
 resource "aws_instance" "web2" {
   ami                    = "ami-0f58b397bc5c1f2e8" #change your ami value according to your aws instance 
   instance_type          = "t2.medium"
-  key_name               = "maylatest"
+  key_name               = "" #your key name here
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   tags = {
     Name = "Grafana"
